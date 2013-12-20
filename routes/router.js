3483c85ba22fba5c72475c21e55c07813a7f05d8
ZECTBynmo <michael.vegeto@gmail.com>
2013-12-19 22:00:28 -0500

    Add in our csv file back end

--- routes/router.js
+++ routes/router.js
@@ -2,10 +2,30 @@
 require('json5/lib/require');
 
 // leto-marker-router-requires
-var config = require("../config"),
+var lineReader = require('line-reader'),
+    config = require("../config"),
 	uuid = require("node-uuid"),
+    path = require('path'),
 	fs = require("fs");
 
+var recordsFile = "./records.csv",
+    records = [];
+
+loadCSV( recordsFile, function(contents) {
+    console.log( "Loaded " + contents.length + " records" );
+    records = contents;
+});
+
+// enum record indices
+var INDEX_NAME = 0,
+    INDEX_EMAIL = 1,
+    INDEX_NOISE = 2,
+    INDEX_URL = 3;
+
+// If our records don't exist yet, create them
+if( !path.existsSync(recordsFile) ) {
+    fs.writeFileSync( recordsFile, "" );
+}
 
 //////////////////////////////////////////////////////////////////////////
 // Constructor
@@ -20,16 +40,18 @@ Router.prototype.adminpanel = function( req, res ) {
     // We don't want the browser to cache the results 
     res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
 
-    res.json( 200, {} );
+    res.json( 200, records );
 };
 
 
 // POST /upload
 Router.prototype.upload = function( req, res ) {
+    var _this = this;
     
     var data = req.body,
+        params = JSON.parse(req.body.params),
     	uploadID = uuid.v1(),
-    	fileExtension = ".zip",
+    	fileExtension = "zip",
         fileBuffer;
 
     var outputPath = config.upload_dir + '/' + uploadID + "." + fileExtension;
@@ -39,8 +61,6 @@ Router.prototype.upload = function( req, res ) {
         fs.mkdirSync( config.upload_dir );
     }
 
-//    fileBuffer = new Buffer( data.file, "base64" );
-
     var zipFile = req.files["files.zip"];
 
     fs.readFile( zipFile.path, function( err, data ) {
@@ -49,9 +69,55 @@ Router.prototype.upload = function( req, res ) {
                 guid: uploadID
             };
 
+            params.url = params.origin + "/" + uploadID + ".zip";
+
+            addRecordItem( params );
+
             res.json( 200, successData );
         });
     });
 }
+
+
+function addRecordItem( record ) {
+    var recordArray = [];
+
+    recordArray[INDEX_NAME] = record.name;
+    recordArray[INDEX_EMAIL] = record.email;
+    recordArray[INDEX_NOISE] = record.noise;
+    recordArray[INDEX_URL] = record.url;
+
+    records.push( recordArray );
+
+    writeCSV( recordsFile, records );
+}
+
+
+function loadCSV( filePath, callback ) {
+    contents = [];
+
+    // Load up our CSV file
+    lineReader.eachLine( filePath, function(line, last) {
+        contents.push( line.split(",") );
+
+        if( last ) {
+            callback( contents );
+        }
+    });
+}
+
+
+function writeCSV( filePath, contents ) {
+    var strContents = "";
+
+    for( var iItem in contents ) {
+        strContents += contents[iItem].join() + "\n";
+    }
+
+    fs.writeFileSync( filePath, strContents );
+
+    console.log( "Updated CSV" );
+}
+
 
 exports.Router = Router;
